57b2012bf11fecf00eb05178ec02daec23c7f11d
Snigdhabhatnagar <snigdha.bhatnagar312@gmail.com>
2018-12-17 18:29:15 +0530

    routing

---
+++ src/Container/About/About.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+	render() {
+		return (
+			<div><h1>About canVisaGuide</h1>
+			 <p>checking about</p>
+        <p>checking about</p>
+        <p>checking about</p>
+        <p>checking about</p>
+        <p>checking about</p>
+        <p>checking about</p>
+        <p>checking about</p>
+        <p>checking about</p>
+        <p>checking about</p>
+        <p>checking about</p>
+        <p>checking about</p>
+        <p>checking about</p>
+        <p>checking about</p>
+        <p>checking about</p>
+        <p>checking about</p>
+        <p>checking about</p>
+        <p>checking about</p>
+        <p>checking about</p>
+        <p>checking about</p>
+        </div>);
+	}
+}
+export default About;
