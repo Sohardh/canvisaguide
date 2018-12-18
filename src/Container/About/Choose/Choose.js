b16e0532085ebe4bc9dc72b87efb39efd723169f
Sohardh <31403178+Sohardh@users.noreply.github.com>
2018-12-18 11:13:17 +0530

    Added about us and profile

---
+++ src/Container/About/Choose/Choose.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Choose extends Component {
+	render() {
+		return (
+			<div><h1>Choose canVisaGuide</h1>
+			 <p>checking Choose</p>
+        <p>checking Choose</p>
+        <p>checking Choose</p>
+        <p>checking Choose</p>
+        <p>checking Choose</p>
+        <p>checking Choose</p>
+        <p>checking Choose</p>
+        <p>checking Choose</p>
+        <p>checking Choose</p>
+        <p>checking Choose</p>
+        <p>checking Choose</p>
+        <p>checking Choose</p>
+        <p>checking Choose</p>
+        <p>checking Choose</p>
+        <p>checking Choose</p>
+        <p>checking Choose</p>
+        <p>checking Choose</p>
+        <p>checking Choose</p>
+        <p>checking Choose</p>
+        </div>);
+	}
+}
+export default Choose;
